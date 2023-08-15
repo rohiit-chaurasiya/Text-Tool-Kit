4d703264dce1acbd64d613cf8ec7d025b45ff368
Rohit Chaurasia <rohiit.chaurasiya@gmail.com>
2023-08-16 03:43:08 +0530

    Update Auth.jsx

--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -24,7 +24,7 @@ const Auth=()=> {
         
         e.preventDefault();
         
-        const response=await fetch('http://localhost:3001/signin',{
+        const response=await fetch('https://text-tool-kit-backend.vercel.app/signin',{
             method:'POST',
             body:JSON.stringify(form),
             headers:{
