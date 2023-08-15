8ffa06247d5384437473754cc8a8f691916d64df
Rohit Chaurasia <rohiit.chaurasiya@gmail.com>
2023-08-16 04:24:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const RegisterModel = require('./models/Register')
 
 const app = express()
 app.use(cors({
-    origin: ['https://text-toolkit.vercel.app'],
+    origin: ['https://text-toolkit.vercel.app/'],
     methods: ['POST', 'GET'],
     credentials: true
 }));
